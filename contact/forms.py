36e0bff25c24c488ba986c202105d79ce0bb177d
Pete <pgcookson@gmail.com>
2024-06-27 12:08:46 +0100

    update to success/error modal

--- contact/forms.py
+++ contact/forms.py
@@ -1,13 +1,20 @@
+import re
 from django import forms
 from django.core.validators import EmailValidator, MinLengthValidator
 from django.core.exceptions import ValidationError
 from .constants import VALID_POSTCODES
 from .models import Contact
 
+POSTCODE_REGEX = re.compile(r'^[A-Z]{2}\d{2,}[A-Z]{2}$')
+UK_PHONE_REGEX = re.compile(r'^(\+44\s?7\d{3}|\(?07\d{3}\)?)\s?\d{3}\s?\d{3}$|^(\+44\s?1\d{3}|\(?01\d{3}\)?)\s?\d{3}\s?\d{3}$|^(\+44\s?2\d{3}|\(?02\d{3}\)?)\s?\d{3}\s?\d{3}$')
+
+
 def validate_postcode(value):
     normalized_value = value.replace(" ", "").upper()  # Remove spaces and convert to uppercase
-    if normalized_value not in VALID_POSTCODES:
-        raise ValidationError('Invalid postcode')
+    if not POSTCODE_REGEX.match(normalized_value):  # Check if the postcode matches the pattern
+        raise ValidationError('Incorrect postcode, please enter a valid postcode')
+    if normalized_value not in VALID_POSTCODES:  # Check if the postcode is not covered
+        raise ValidationError('Sorry, we do not currently cover this postcode.')
 
 class ContactForm(forms.ModelForm):
     class Meta:
@@ -42,6 +49,8 @@ class ContactForm(forms.ModelForm):
 
     def clean_phone_number(self):
         phone_number = self.cleaned_data['phone_number']
+        if not UK_PHONE_REGEX.match(phone_number):
+            raise ValidationError('Invalid phone number. Please enter a valid phone number.')
         return phone_number
 
     def clean_subject(self):
